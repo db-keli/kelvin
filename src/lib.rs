a69b604aa231ae058d63f72ad32066a89d043b86
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-03-24 18:42:19 +0000

    chore: organization

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use rand::thread_rng;
+use rand::Rng;
+
+pub fn generate_password(length: usize) -> String {
+    let ascii_chars: Vec<char> = (33..=126).map(|c| c as u8 as char).collect();
+    let mut rng = thread_rng();
+
+    (0..length)
+        .map(|_| {
+            let idx = rng.gen_range(0..ascii_chars.len());
+            ascii_chars[idx]
+        })
+        .collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::{thread_rng, Rng};
+use kelvin::generate_password;
 
 fn main() {
     let length = 15;
@@ -6,16 +6,4 @@ fn main() {
     let pass = generate_password(length);
 
     println!("The password is {}", pass);
-}
-
-fn generate_password(length: usize) -> String {
-    let ascii_chars: Vec<char> = (33..=126).map(|c| c as u8 as char).collect();
-    let mut rng = thread_rng();
-
-    (0..length)
-        .map(|_| {
-            let idx = rng.gen_range(0..ascii_chars.len());
-            ascii_chars[idx]
-        })
-        .collect()
 }
