1df8586d31c5f21f293ccbde12bf40902a73e97c
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-03-24 20:58:55 +0000

    chore: wrote test for password_generate

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,24 @@ use rand::Rng;
 pub fn generate_password(length: usize) -> String {
     let ascii_chars: Vec<char> = (33..=126).map(|c| c as u8 as char).collect();
     let mut rng = thread_rng();
-
     (0..length)
         .map(|_| {
             let idx = rng.gen_range(0..ascii_chars.len());
             ascii_chars[idx]
         })
         .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn password_length() {
+        let ascii_chars: Vec<char> = (33..=126).map(|c| c as u8 as char).collect();
+        let length = 7;
+        let password_vector: Vec<char> = generate_password(length).chars().collect();
+
+        assert!(password_vector.iter().all(|&x| ascii_chars.contains(&x)));
+    }
 }
