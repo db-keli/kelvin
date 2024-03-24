903869d6dee09aa304e44f9df3b0dc37d71aa51c
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-03-24 17:06:26 +0000

    chore: add generate_password function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rand::{thread_rng, Rng};
+
 fn main() {
-    println!("Hello, world!");
+    let length = 15;
+
+    let pass = generate_password(length);
+
+    println!("The password is {}", pass);
+}
+
+fn generate_password(length: usize) -> String {
+    let ascii_chars: Vec<char> = (33..=126).map(|c| c as u8 as char).collect();
+    let mut rng = thread_rng();
+
+    (0..length)
+        .map(|_| {
+            let idx = rng.gen_range(0..ascii_chars.len());
+            ascii_chars[idx]
+        })
+        .collect()
 }
