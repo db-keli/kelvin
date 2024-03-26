3f0576fc74dba34f3fed40dadf7a2cd2aaf05d39
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-03-26 23:20:01 +0000

    hashes will never be the same though

--- src/lib.rs
+++ src/lib.rs
@@ -114,6 +114,18 @@ mod test {
     }
 
     #[test]
+    fn test_hashing() {
+        let password: String = generate_password(12);
+        let name: String = String::from("Michael");
+        let mut admin: Admin = Admin::new(&name, &password);
+
+        admin.hash_password();
+        let hashed_password = hash(&password, DEFAULT_COST).expect("Failed to hash password");
+
+        assert_ne!(admin.password, hashed_password);
+    }
+
+    #[test]
     fn test_verify_function() {
         let password: String = generate_password(12);
         let name: String = String::from("Michael");
