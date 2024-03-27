1f56102d49964f3d7e224259281a321f02dcf2ba
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-03-27 16:06:34 +0000

    chore: encryption method done, yet to test

--- src/deck/deck.rs
+++ src/deck/deck.rs
@@ -1,10 +1,22 @@
-//use aes_gcm::{Aes256Gcm, Key, Nonce, Tag};
+use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+use rand::{thread_rng, rngs::ThreadRng};
+
 
 //Add a deck
 pub struct Deck {
     pub domain: String,
     pub plaintext: String,
 }
+
+pub fn get_keys() -> (RsaPrivateKey, RsaPublicKey, ThreadRng){
+    let mut rng = thread_rng();
+    let bits = 2048;
+    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("Failed to generate a key");
+    let public_key = RsaPublicKey::from(&private_key); 
+    
+    (private_key, public_key, rng)
+}
+
 
 impl Deck {
     pub fn new(domain: &str, plaintext: &str) -> Deck {
@@ -13,6 +25,15 @@ impl Deck {
 
         Deck { domain, plaintext }
     }
+    
+    pub fn encrypt(&self) -> Vec<u8>{
+        let mut keys = get_keys();
 
-    pub fn _encrypt(&self) {}
+        let plaintext = &self.plaintext.as_bytes();
+
+        let encrypted_data = keys.1.encrypt(&mut keys.2, Pkcs1v15Encrypt, &plaintext[..]).expect("Failed to encrypt");
+
+        encrypted_data
+    }
 }
+
---
+++ tests/deck.rs
@@ -0,0 +1,11 @@
+use kelvin::deck::deck::*;
+use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+use rand::{random, rngs::ThreadRng, thread_rng};
+
+let deck1 = 
+#[test]
+fn test_get_keys(){
+    let mut keys = get_keys();
+
+    assert!()
+}
