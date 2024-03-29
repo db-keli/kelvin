85267d97cd576289c3dc79305fc79648d9c86a22
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-03-29 10:50:22 +0000

    refactor

--- src/deck/deck.rs
+++ src/deck/deck.rs
@@ -1,6 +1,5 @@
+use rand::{rngs::ThreadRng, thread_rng};
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
-use rand::{thread_rng, rngs::ThreadRng};
-
 
 //Add a deck
 pub struct Deck {
@@ -8,15 +7,14 @@ pub struct Deck {
     pub plaintext: String,
 }
 
-pub fn get_keys() -> (RsaPrivateKey, RsaPublicKey, ThreadRng){
+pub fn get_keys() -> (RsaPrivateKey, RsaPublicKey, ThreadRng) {
     let mut rng = thread_rng();
     let bits = 2048;
     let private_key = RsaPrivateKey::new(&mut rng, bits).expect("Failed to generate a key");
-    let public_key = RsaPublicKey::from(&private_key); 
-    
+    let public_key = RsaPublicKey::from(&private_key);
+
     (private_key, public_key, rng)
 }
-
 
 impl Deck {
     pub fn new(domain: &str, plaintext: &str) -> Deck {
@@ -25,20 +23,27 @@ impl Deck {
 
         Deck { domain, plaintext }
     }
-    
+
     pub fn encrypt(&self) -> (Vec<u8>, (RsaPrivateKey, RsaPublicKey, ThreadRng)) {
         let mut keys = get_keys();
 
         let plaintext = &self.plaintext.as_bytes();
 
-        let encrypted_data = keys.1.encrypt(&mut keys.2, Pkcs1v15Encrypt, &plaintext[..]).expect("Failed to encrypt");
+        let encrypted_data = keys
+            .1
+            .encrypt(&mut keys.2, Pkcs1v15Encrypt, &plaintext[..])
+            .expect("Failed to encrypt");
 
         (encrypted_data, keys)
     }
 
     pub fn decrypt(&self) -> Vec<u8> {
         let encrypted_data = self.encrypt();
-        let dec_data = encrypted_data.1.0.decrypt(Pkcs1v15Encrypt, &encrypted_data.0).expect("failed to decrypt");
+        let dec_data = encrypted_data
+            .1
+             .0
+            .decrypt(Pkcs1v15Encrypt, &encrypted_data.0)
+            .expect("failed to decrypt");
 
         dec_data
     }
