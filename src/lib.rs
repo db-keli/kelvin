be433a10ca36daebee315c28251aba303f65132c
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-04-07 10:26:56 +0000

    chore: prompt for deck

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,25 @@ pub fn prompt_password(prompt: &str) -> Result<String> {
     Ok(password)
 }
 
+pub fn prompt_deck() -> Result<(String, String)>{
+    let _ = stdout().flush();
+
+    print!("Enter domain:");
+    stdout().flush()?;
+
+    let mut username = String::new();
+    stdin()
+        .read_line(&mut username)
+        .expect("Failed to read line");
+    username = username.trim().to_string();
+    print!("Enter password:");
+    stdout().flush()?;
+    let password = rpassword::read_password()?;
+    print!("");
+    println!(); 
+    
+    Ok((username, password))
+}
 
 pub fn prompt_logins() -> Result<(String, String)> {
     let _ = stdout().flush();
