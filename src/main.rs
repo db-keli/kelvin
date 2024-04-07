bbc4ef5d997071ce1750f433bc74cf70c8d74830
Dompeh Kofi Bright <kekelidompeh@gmail.com>
2024-04-07 10:27:29 +0000

    add deck command functional

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,30 @@ mod admin;
 mod deck;
 mod deckdata;
 
-use std::io::{ErrorKind, Error};
+use std::io::{Error, ErrorKind};
 
 use admin::admin::Admin;
+use bcrypt::{hash, DEFAULT_COST};
 use clap::Command;
 use deck::deck::Deck;
 use deckdata::deckdata::DeckData;
-use kelvin::{generate_password, prompt_logins, check_file_exists1, read_user_data};
-use bcrypt::{hash, DEFAULT_COST};
+use kelvin::{check_file_exists1, generate_password, prompt_logins, read_user_data, prompt_deck};
 
 fn main() {
     let matches = Command::new("kelvin")
         .version("0.0.1")
         .author("Dompeh Kofi Bright, kekelidompeh@gmail.com")
         .about("A password managements system, more of a vault admin")
-        .subcommand(
-            Command::new("create-admin").about("Creates an admin account")
-        )
-        .subcommand(
-            Command::new("deck")
-                .about("Adds a deck to the vault")
-        )
+        .subcommand(Command::new("create-admin").about("Creates an admin account"))
+        .subcommand(Command::new("deck").about("Adds a deck to the vault"))
         .subcommand(Command::new("reset").about("Resets the vault"))
         .get_matches();
 
     let mut status: Option<bool> = None;
-    let mut admin :Option<Admin> = None;
-    
+    let mut admin: Option<Admin> = None;
+
     if let Some(matches) = matches.subcommand_matches("create-admin") {
-        let logins  = prompt_logins().unwrap();
+        let logins = prompt_logins().unwrap();
         let admin_password = logins.1;
         let mut admin = Admin::new(&logins.0, &admin_password);
         admin.hash_password();
@@ -46,14 +41,17 @@ fn main() {
             print!("{}", err);
         } else {
             let status = status.unwrap();
-            if status.prompt_auth(username, password).unwrap(){
-                print!("You're authorized");
+            if status.prompt_auth(username, password).unwrap() {
+                println!("Add a deck to the Vault.");
+                let deck_data = prompt_deck().unwrap();
+                let deck = Deck::new(&deck_data.0, &deck_data.1);
+                let encrypted_data = deck.encrypt();
+                let deck_data = DeckData::new(status, deck.domain, encrypted_data.0, encrypted_data.1 .1, encrypted_data.1 .0);
+                deck_data.save_to_json().unwrap();                
             } else {
                 println!("You're unathorized");
             }
         }
-
-
     } else if let Some(matches) = matches.subcommand_matches("reset") {
         //Reset the vault
         status = Some(true);
@@ -93,4 +91,3 @@ fn main() {
     //     String::from_utf8_lossy(&deck_data_buf.decrypt())
     //);
 }
-
