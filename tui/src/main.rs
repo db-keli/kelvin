fd064a960092334c3bd4e2822d053d55d07c9929
g4titanx <g4titan1@gmail.com>
2024-07-04 10:07:19 +0100

    update ratatui

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::io::{self, stdout, Stdout};
+
+use ratatui::{
+    backend::CrosstermBackend,
+    crossterm::{
+        execute,
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    },
+    Terminal,
+};
