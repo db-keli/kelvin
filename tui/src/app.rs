ad7f409081c1e0afd429d083a179e02c44acb48b
g4titanx <g4titan1@gmail.com>
2024-07-10 00:06:35 +0100

    improv: editing app states

--- tui/src/app.rs
+++ tui/src/app.rs
@@ -1,13 +1,20 @@
-enum MainmenuState{
+enum MainmenuState {
     GenPasswd,
     CreateAdmin,
     AddDeck,
     OpenSesame,
-    Reset
+    Reset,
+}
+
+struct GenPasswd {
+    // Define fields needed for generating passwords
+    length: usize,
 }
 
 enum StartupState {
-    
+    Initialize,
+    CheckAdmin,
+    UnlockVault,
 }
 
 pub struct Admin {
@@ -15,17 +22,20 @@ pub struct Admin {
     pub password: String,
 }
 
-pub struct  App {
-    pub Startup: StartupState,
-    pub Mainmenu: MainmenuState,
-
-
+pub struct App {
+    pub startup: StartupState,
+    pub mainmenu: MainmenuState,
+    pub admin: Option<Admin>, // Admin details, if any
+    pub gen_passwd: Option<GenPasswd>, // State for generating password
 }
+
 impl App {
     pub fn new() -> App {
         App {
-            Startup,
-            Mainmenu,
+            startup: StartupState::Initialize,
+            mainmenu: MainmenuState::GenPasswd,
+            admin: None,
+            gen_passwd: None,
         }
     }
 }
