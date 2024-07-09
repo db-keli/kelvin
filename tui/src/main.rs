11023cfc78688da993c008fdd181e4322897d9cb
g4titanx <g4titan1@gmail.com>
2024-07-09 16:31:51 +0100

    feat:terminal setup

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,10 +1,41 @@
-use std::io::{self, stdout, Stdout};
+use std::error::Error;
+use std::io::{self, stdout};
 
 use ratatui::{
-    backend::CrosstermBackend,
+    backend::{Backend, CrosstermBackend},
     crossterm::{
+        event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
         execute,
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     },
     Terminal,
 };
+
+mod app;
+mod ui;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    // Switch to the alternate screen and enable mouse capture 
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let mut app = app::App::new();
+    let res = ui::run_app(&mut terminal, &mut app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen, DisableMouseCapture)?;
+    terminal.show_cursor()?;
+
+    // check for app run result
+    if let Err(err) = res {
+        eprintln!("Error: {}", err);
+    }
+
+    Ok(())
+}
